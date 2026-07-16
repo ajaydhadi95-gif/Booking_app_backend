8d618fa8a9093869f559555d782711fd14ed8d60
Ajay Dhadi <ajaydhadi95@gmail.com>
2026-07-16 16:02:26 +0530

    file modified

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
+const User = require('../models/User');
 
 // 1. REGISTER ROUTE (Sign up)
 router.post('/register', async (req, res) => {
